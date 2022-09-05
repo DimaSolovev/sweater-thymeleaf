89518aad48e42399512bf35e655ce29a38eefc9c
Dima <soloveid87@mail.ru>
2022-09-05 22:32:25 +1000

    page and size work

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -20,8 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping
@@ -50,14 +50,13 @@ public class MainController {
             @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
     ) {
         Page<Message> page;
-        Iterable<Message> messages = messageRepository.findAll();
 
         if (filter != null && !filter.isEmpty()) {
-            page = messageRepository.findByTag(filter,pageable);
+            page = messageRepository.findByTag(filter, pageable);
         } else {
             page = messageRepository.findAll(pageable);
         }
-
+        model.addAttribute("url", "/main");
         model.addAttribute("page", page);
         model.addAttribute("filter", filter);
         return "main";
@@ -142,5 +141,29 @@ public class MainController {
             messageRepository.save(message);
         }
         return "redirect:/user-messages/" + user.getId();
+    }
+
+    static int[] computePagination(Page page){
+        Integer totalPages=page.getTotalPages();
+        if(totalPages>7) {
+            Integer pageNumber = page.getNumber() + 1;
+            Integer[] head = pageNumber > 4 ? new Integer[]{1, -1} : new Integer[]{1, 2, 3};
+            Integer[] tail = pageNumber < (totalPages - 3) ? new Integer[]{-1, totalPages} : new Integer[]{totalPages - 2, totalPages - 1, totalPages};
+            Integer[] bodyBefore = (pageNumber > 4 && pageNumber < (totalPages - 1)) ? new Integer[]{pageNumber - 2, pageNumber - 1} : new Integer[]{};
+            Integer[] bodyAfter = (pageNumber > 2 && pageNumber < (totalPages - 3)) ? new Integer[]{pageNumber + 1, pageNumber + 2} : new Integer[]{};
+
+            List<Integer> list = new ArrayList<>();
+            Collections.addAll(list, head);
+            Collections.addAll(list, bodyBefore);
+            Collections.addAll(list, (pageNumber > 3 && pageNumber < totalPages - 2) ? new Integer[]{pageNumber} : new Integer[]{});
+            Collections.addAll(list, bodyAfter);
+            Collections.addAll(list, tail);
+            Integer[] arr= list.toArray(new Integer[0]);
+            int[] res=Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
+            return res;
+        }else{
+            return IntStream.rangeClosed(1, totalPages).toArray();
+        }
+
     }
 }
